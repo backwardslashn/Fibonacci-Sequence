be77fed728cb78b287cc38453aef1963e010970e
backwardslashn <backwardslashn@gmail.com>
2025-01-28 00:26:49 -0800

    Add files via upload

---
+++ Fib.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/* fibonacci sequence; 0 to 255 */
+
+int main()
+{
+    int x;
+    int y;
+    int z;
+
+    while (1) 
+    {
+        x = 0;
+        y = 1;
+        do {
+            printf("%d\n", x);
+
+            z = y + y;
+            x = y;
+            y = z;
+        } while (x < 255);
+    }
+}
